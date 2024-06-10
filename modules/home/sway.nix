459355f408e6a49b5a533935d1166172ed75f9ec
highghlow <highghlow@proton.me>
2024-06-10 13:19:38 +0300

    Removed .alock-status

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -116,21 +116,6 @@ in
           format = "{:%H(%I):%M}";
         };
 
-        "custom/autolock-status" = {
-          format = "{}";
-          return-type = "json";
-          exec = pkgs.writeShellScript "waybar-autolock-status" ''
-            while true; do
-              if ps -e | grep swayidle > /dev/null; then
-                echo '{"text": "LCK: active", "class": ["alock-active", "alock-status"]}'
-              else
-                echo '{"text": "LCK: stopped", "class": ["alock-stopped", "alock-status"]}'
-              fi
-              sleep 1
-            done;
-          '';
-        };
-
         "custom/date" = {
           format = "{}";
           return-type = "json";
