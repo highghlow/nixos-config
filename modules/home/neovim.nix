805acf7579eacf893d1e844844f0b66210b37902
highghlow <highghlow@proton.me>
2024-06-12 21:43:18 +0300

    Added proper luasnip keybindings

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -85,24 +85,43 @@
 	  sources =
 	    [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
 	  mapping = {
-	    "<M-CR>" = "cmp.mapping.confirm({ select = true })";
-	    "<C-Space>" = "cmp.mapping.complete()";
+	    "<M-CR>" = ''
+cmp.mapping(function(fallback)
+    if cmp.visible() then
+	if luasnip.expandable() then
+	    luasnip.expand()
+	else
+	    cmp.confirm({
+		select = true,
+	    })
+	end
+    else
+	fallback()
+    end
+end)
+'';
 	    "<Tab>" = ''
-  cmp.mapping(function(fallback)
-      if cmp.visible() then
-	  cmp.select_next_item()
-      else
-	  fallback()
-      end
-  end, {'i', 's'})'';
+cmp.mapping(function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif luasnip.locally_jumpable(1) then
+    luasnip.jump(1)
+  else
+    fallback()
+  end
+end, { "i", "s" })
+'';
 	    "<S-Tab>" = ''
-  cmp.mapping(function(fallback)
-      if cmp.visible() then
-	  cmp.select_prev_item()
-      else
-	  fallback()
-      end
-  end, {'i', 's'})'';
+cmp.mapping(function(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.locally_jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end, { "i", "s" })
+'';
 	  };
 	};
       };
