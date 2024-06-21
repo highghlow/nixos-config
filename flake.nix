af357bbdc43826625c07016b0abe97657bed4558
highghlow <highghlow@proton.me>
2024-06-21 21:17:37 +0300

    Added host: floatware

--- flake.nix
+++ flake.nix
@@ -13,9 +13,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix.url = "github:danth/stylix";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, stylix, nixvim, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nur, stylix, nixvim, nixos-generators, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
 	system = "x86_64-linux";
@@ -39,6 +43,30 @@
 	  }
 	];
       };
+    };
+    packages.x86_64-linux.floatware = nixos-generators.nixosGenerate {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+	nixos-generators.nixosModules.iso
+
+	nur.nixosModules.nur
+	stylix.nixosModules.stylix
+
+	./colorscheme.nix
+	./hosts/floatware/configuration.nix
+
+	home-manager.nixosModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.sharedModules = [
+	    nixvim.homeManagerModules.nixvim
+	    nur.nixosModules.nur
+	  ];
+	  home-manager.users.nixer = import ./hosts/floatware/home.nix;
+	}
+      ];
+      format = "iso";
     };
   };
 }
