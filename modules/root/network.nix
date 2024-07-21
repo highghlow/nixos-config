599dd0e09e6f6e1c51ce99e6d0b05919b7c9b5be
highghlow <highghlow@proton.me>
2024-07-21 12:22:45 +0300

    Wifi support

--- modules/root/network.nix
+++ modules/root/network.nix
@@ -4,15 +4,23 @@
   options = {
     mynixos.network.enable =
       lib.mkEnableOption "Enable module";
+    mynixos.network.wireless =
+      lib.mkEnableOption "Enable wireless";
   };
 
-  config = lib.mkIf config.mynixos.network.enable {
-    networking.networkmanager.enable = true;
-    networking.firewall = {
-      enable = true;
-      allowedTCPPortRanges = [ 
-	{ from = 5900; to = 5999; }
-      ];
-    };
-  };
+  config = let cfg = config.mynixos.network;
+  in lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      networking.networkmanager.enable = true;
+      networking.firewall = {
+	enable = true;
+	allowedTCPPortRanges = [ 
+	  { from = 5900; to = 5999; }
+	];
+      };
+    }
+    (lib.mkIf cfg.wireless {
+      networking.wireless.iwd.enable = true;
+    })
+  ]);
 }
