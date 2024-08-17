e841063b8bc1fe16c481345e76ab8b58a619cc76
highghlow <highghlow@proton.me>
2024-08-17 17:19:42 +0300

    lazygit

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -2,18 +2,27 @@
 
 {
   options = {
-    mynixos.home.git.enable =
-      lib.mkEnableOption "Enable module";
-  };
-
-  config = lib.mkIf config.mynixos.home.git.enable {
-    programs.git = {
-      enable = true;
-      userName = "highghlow";
-      userEmail = "highghlow@proton.me";
-      extraConfig = {
-	init.defaultBranch = "main";
-      };
+    mynixos.home.git = {
+      enable =
+	lib.mkEnableOption "Enable module";
+      lazygit = lib.mkOption { type = lib.types.bool; default = true; };
     };
   };
+
+  config = let cfg = config.mynixos.home.git; in
+  lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      programs.git = {
+	enable = true;
+	userName = "highghlow";
+	userEmail = "highghlow@proton.me";
+	extraConfig = {
+	  init.defaultBranch = "main";
+	};
+      };
+    }
+    (lib.mkIf cfg.lazygit {
+      home.packages = [ pkgs.lazygit ];
+    })
+  ]);
 }
