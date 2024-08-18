81d978fb8d3ac3284a71fe709a40ae3f23045bd8
highghlow <highghlow@proton.me>
2024-08-18 19:06:52 +0300

    No forkgram

--- modules/home/telegram.nix
+++ modules/home/telegram.nix
@@ -1,17 +1,5 @@
 { pkgs, lib, config, ... }:
 
-let forkgram = pkgs.telegram-desktop.overrideAttrs (old: rec {
-    name = "forkgram";
-    version = "5.1.8";
-    src = pkgs.fetchFromGitHub {
-	    owner = "forkgram";
-	    fetchSubmodules = true;
-	    repo = "tdesktop";
-	    rev = "v${version}";
-	    hash = "sha256-ZLzJak6Rn5/Ys9qTQLdqX0FqwXe3OmjgLSvZPSRloZ8=";
-    };
-});
-in
 {
   options = {
     mynixos.home.telegram.enable =
@@ -19,6 +7,6 @@ in
   };
 
   config = lib.mkIf config.mynixos.home.telegram.enable {
-    home.packages = [ forkgram ];
+    home.packages = [ pkgs.telegram-desktop ];
   };
 }
