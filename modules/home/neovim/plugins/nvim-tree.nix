1d24b62a6981728ade259bb1fc7d27419f464387
highghlow <highghlow@proton.me>
2024-09-02 17:56:11 +0000

    Nvim-tree config

--- modules/home/neovim/plugins/nvim-tree.nix
+++ modules/home/neovim/plugins/nvim-tree.nix
@@ -6,6 +6,40 @@
       vim.cmd([[cnoreabbrev nt NvimTreeFocus]])
     '';
 
-    plugins.nvim-tree.enable = true;
+    plugins.nvim-tree = {
+      enable = true;
+      hijackUnnamedBufferWhenOpening = true;
+      filters = { custom = [ "^.git$" ]; };
+      diagnostics = {
+        enable = true;
+        showOnDirs = true;
+        showOnOpenDirs = false;
+      };
+      git = {
+        enable = true;
+        showOnDirs = true;
+        showOnOpenDirs = false;
+        ignore = false;
+      };
+      updateFocusedFile.enable = true;
+      view = {
+        float.enable = true;
+        centralizeSelection = true;
+        cursorline = true;
+      };
+      onAttach = {__raw = ''
+        function(bufnr)
+          local api = require "nvim-tree.api"
+          api.config.mappings.default_on_attach(bufnr)
+
+          vim.keymap.set('n', '<Esc>', function() 
+            local win_amount = #vim.api.nvim_tabpage_list_wins(0)
+            if win_amount > 1 then
+              return '<Cmd>:q<CR>'
+            end
+          end, { silent = true, buffer = bufnr, expr = true })
+        end
+      ''; };
+    };
   };
 }
